ce972b79b6228178c6d6ac37f55ec8a4e6c2b121
Syawal Ludin <syawalludin2003@gmail.com>
2022-12-03 01:46:13 +0700

    Update file

--- src/test/java/bengCall/admin/adminAPI.java
+++ src/test/java/bengCall/admin/adminAPI.java
@@ -7,26 +7,9 @@ public class adminAPI {
     public static final String POST_LOGIN_ADMIN = BASE_URL+"/login";
     public static final String GET_ALL_VEHICLE_ADMIN = BASE_URL+"/vehicle";
     public static final String GET_ALL_VEHICLE_ADMIN_INVALID_PATH = BASE_URL+"/vehicless";
-    public static final String POST_CREATE_VEHICLE_ADMIN =BASE_URL +"/admin/vehicle";
-    public static final String DELETE_VEHICLE_TYPE_ADMIN = BASE_URL+"/admin/vehicle/{id}";
-    public static final String DELETE_VEHICLE_TYPE_ADMIN_NO_ID = BASE_URL+"/admin/vehicle";
-    public static final String DELETE_VEHICLE_TYPE_ADMIN_SPEC_CHARACTER = BASE_URL+"/admin/vehicle/{character}";
-    public static final String GET_ALL_SERVICE_TYPE_ADMIN = BASE_URL+"/service/{id}";
-    public static final String GET_ALL_SERVICE_TYPE_ADMIN_SPEC_CHARACTER = BASE_URL + "/service/{character}";
-    public static final String POST_CREATE_SERVICE_ADMIN = BASE_URL+"/admin/service";
-    public static final String DELETE_SERVICE_ADMIN = BASE_URL+"/admin/service/{id}";
-    public static final String DELETE_SERVICE_ADMIN_NO_ID = BASE_URL+"/admin/service";
-    public static final String DELETE_SERVICE_ADMIN_SPEC_CHARACTER = BASE_URL+"/admin/service/{character}";
     public static final String GET_DETAIL_TRANSACTION_ADMIN = BASE_URL+"/transaction/{id}";
     public static final String GET_DETAIL_TRANSACTION_ADMIN_INVALID_PATH = BASE_URL+"/transactionss";
     public static final String GET_DETAIL_TRANSACTION_ADMIN_SPEC_CHAR = BASE_URL + "/transaction/{character}";
-    public static final String GET_TRANSACTION_ADMIN_VALID_PATH = BASE_URL+ "/admin/transaction";
-    public static final String GET_TRANSACTION_ADMIN_INVALID_PATH = BASE_URL+ "/admin/transaction/dhuaar";
-    public static final String PUT_TRANSACTION_ADMIN  = BASE_URL+ "/admin/transaction/1";
-    public static final String PUT_TRANSACTION_ADMIN_INVALID = BASE_URL+ "/admin/transaction/HAHA";
-    public static final String DELETE_CANCEL_TRANSACTION_ADMIN = BASE_URL+ "/admin/transaction/{id}";
-    public static final String GET_VEHICLESERVICE_ADMIN = BASE_URL + "/admin/vehicleservice";
-    public static final String GET_VEHICLESERVICE_INVALID_PATH_ADMIN = BASE_URL + "/admin/vehicleservicess";
-    public static final String VALID_ADMIN_TOKEN = "";
+    public static final String VALID_ADMIN_TOKEN = "Bearer ";
     public static final String INVALID_ADMIN_TOKEN = "Bearer ";
 }
