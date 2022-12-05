a3994575b0eb46858a0053e9fa3ea96afcf00061
Syawal Ludin <syawalludin2003@gmail.com>
2022-12-06 04:32:08 +0700

    add and update file

--- src/test/java/bengCall/admin/AdminLoginAPI.java
+++ src/test/java/bengCall/admin/AdminLoginAPI.java
@@ -7,23 +7,45 @@ import net.thucydides.core.annotations.Step;
 import java.io.File;
 
 public class AdminLoginAPI {
-    public static final String BASE_URL = "http://project-edu.online";
+    public static final String BASE_URL = "https://project-edu.online";
+
+    //Folder
     public static final String DIR = System.getProperty("user.dir");
     public static final String JSON_FILE = DIR + "/src/test/resources/JsonFile";
     public static final String JSON_REQ_BODY = JSON_FILE + "/JsonRequestBody/Admin/Login";
     public static final String JSON_SCHEMA_VAL = JSON_FILE + "/JsonSchemaValidation/Admin/Login";
 
-
+    //URL
     public static final String POST_LOGIN_ADMIN = BASE_URL + "/login";
+    public static final String INVALID_LOGIN_ADMIN = BASE_URL + "/logi";
 
-
+    //TOKEN
     public static final String VALID_ADMIN_TOKEN = "Bearer ";
     public static final String INVALID_ADMIN_TOKEN = "Bearer ";
 
 
     //Login Admin
-    @Step("Login admin")
-    public void LoginAdmin(File json){
+    @Step("Valid Login admin")
+    public void ValidLoginAdmin(File json){
+        SerenityRest.given().contentType(ContentType.JSON).body(json);
+    }
+
+    //Login Admin with invalid data
+    @Step("invalid login admin")
+    public void InvalidLoginAdmin(File json){
+        SerenityRest.given().contentType(ContentType.JSON).body(json);
+
+    }
+
+    //Login Admin without email
+    @Step("Login admin without email")
+    public void loginAdminWithoutEmail(File json){
+        SerenityRest.given().contentType(ContentType.JSON).body(json);
+    }
+
+    //Login Admin without password
+    @Step("Login admin without password")
+    public void loginAdminWithoutPassword(File json){
         SerenityRest.given().contentType(ContentType.JSON).body(json);
     }
 }
--- src/test/java/bengCall/admin/LoginAdminStepdefs.java
+++ src/test/java/bengCall/admin/LoginAdminStepdefs.java
@@ -4,11 +4,13 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java.eo.Se;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Steps;
 import java.io.File;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 
 public class LoginAdminStepdefs {
 
@@ -16,10 +18,10 @@ public class LoginAdminStepdefs {
     AdminLoginAPI adminLoginAPI;
 
     //Login Admin with Valid data
-    @Given("User login with file json")
-    public void UserLoginWithFileJson(){
+    @Given("User login with valid data file json")
+    public void UserLoginWithValidDataFileJson(){
         File json = new File(AdminLoginAPI.JSON_REQ_BODY + "/LoginAdminRequestBodyJson.json");
-        adminLoginAPI.LoginAdmin(json);
+        adminLoginAPI.ValidLoginAdmin(json);
     }
 
     @When("Send Request admin login")
@@ -39,13 +41,93 @@ public class LoginAdminStepdefs {
                 .body(AdminLoginResponse.MASSAGE, equalTo(massage));
     }
 
-    @And("Validate Login successfully json schema")
+    @And("Validate Login successful json schema")
     public void validateLoginSuccesfullJsonSchema() {
         File json = new File(AdminLoginAPI.JSON_SCHEMA_VAL + "/LoginAdminJsonSchemaValidation.json");
         SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(json));
     }
 
 
+    //Login Admin With Valid data
+    @Given("User login with invalid data file json")
+    public void userLoginWithInvalidDataFileJson() {
+        File json = new File(AdminLoginAPI.JSON_REQ_BODY + "/LoginAdminWithInvalidDataRequestBodyJson.json");
+        adminLoginAPI.InvalidLoginAdmin(json);
+    }
+
+    @Then("Status code {int} Bad request")
+    public void statusCodeBadRequest(int BadRequest) {
+        SerenityRest.then().statusCode(BadRequest);
+    }
+
+    @And("Response body should contain massage {string}")
+    public void responseBodyShouldContainMassage(String massage) {
+        SerenityRest.then().body(AdminLoginResponse.MASSAGE, equalTo(massage));
+    }
+
+    @And("Validate Login admin unsucess json schema")
+    public void validateLoginAdminUnsucessJsonSchema() {
+        File json = new File(AdminLoginAPI.JSON_SCHEMA_VAL + "/InvalidAdminJsonValidation.json");
+        SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(json));
+    }
 
 
+    //invalid Path Login Admin
+    @When("Send Request invalid path admin login")
+    public void sendRequestInvalidPathAdminLogin() {
+        SerenityRest.when().post(AdminLoginAPI.INVALID_LOGIN_ADMIN);
+    }
+
+    @Then("Status code {int} not found")
+    public void statusCodeNotFound(int notFound) {
+        SerenityRest.then().statusCode(notFound);
+    }
+
+    @And("invalid path Response body should contain massage {string}")
+    public void invalidPathResponseBodyShouldContainMassage(String massage) {
+        SerenityRest.then().body(AdminLoginResponse.MASSAGE, equalTo(massage));
+    }
+
+    @And("Validate login admin invalid path")
+    public void validateLoginAdminInvalidPath() {
+        File json = new File(AdminLoginAPI.JSON_SCHEMA_VAL + "/LoginAdminInvalidPathJsonSchmaValidation.json");
+        SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(json));
+    }
+
+    //Login without email
+    @Given("Json File body Without email")
+    public void jsonFileBodyWithoutEmail() {
+        File json = new File(AdminLoginAPI.JSON_REQ_BODY + "/LoginAdminWithoutEmailRequestBodyJson.json");
+        adminLoginAPI.InvalidLoginAdmin(json);
+    }
+
+    @And("response body without email should contain massage {string}")
+    public void responseBodyWithoutEmailShouldContainMassage(String massage) {
+        SerenityRest.then().body(AdminLoginResponse.MASSAGE, equalTo(massage));
+    }
+
+    @And("Validate json path without email")
+    public void validateJsonPathWithoutEmail() {
+        File json = new File(AdminLoginAPI.JSON_SCHEMA_VAL + "/InvalidAdminJsonValidation.json");
+        SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(json));
+    }
+
+    //Login admin without password
+    @Given("Json File body Without password")
+    public void jsonFileBodyWithoutPassword() {
+        File json = new File(AdminLoginAPI.JSON_REQ_BODY + "/LoginAdminWithoutPasswordRequestBodyJson.json");
+        adminLoginAPI.loginAdminWithoutPassword(json);
+    }
+
+
+    @And("response body without password should contain massage {string}")
+    public void responseBodyWithoutPasswordShouldContainMassage(String massage) {
+        SerenityRest.then().body(AdminLoginResponse.MASSAGE, equalTo(massage));
+    }
+
+    @And("Validate json path without password")
+    public void validateJsonPathWithoutPassword() {
+        File json = new File(AdminLoginAPI.JSON_SCHEMA_VAL + "/InvalidAdminJsonValidation.json");
+        SerenityRest.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(json));
+    }
 }
